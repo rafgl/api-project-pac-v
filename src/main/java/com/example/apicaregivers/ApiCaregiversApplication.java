b25fe30ad08f2e296bf2ce05314e2ec76933bc1b
rafgl <rafael.luan.gaedke@hotmail.com>
2021-04-14 18:02:36 -0300

    second commit

---
+++ src/main/java/com/example/apicaregivers/ApiCaregiversApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicaregivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCaregiversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCaregiversApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/apicaregivers/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.example.apicaregivers.repository;
+
+import com.example.apicaregivers.model.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
+
+}
